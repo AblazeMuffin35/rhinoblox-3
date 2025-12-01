de21392f2c9b2f3b544ae4d72ac9a38d10b6613d
Filip Wrochna <wrochna.f@gmail.com>
2025-12-01 20:45:40 +0100

    Vercel Bugfix

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ const workspace = Blockly.inject('blocklyDiv', {
     trashcan: true,
     renderer: 'zelos',
     theme: rhinobloxDarkTheme,
-    media: 'public/blockly_media/',
+    media: '/blockly_media/',
 });
 
 // Create minimap
